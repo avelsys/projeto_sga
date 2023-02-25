afca3e516add4ae4c830ef47cfbde59727f9214a
avelsys <avelinoalessandro@gmail.com>
2023-02-25 02:29:28 -0300

    Update teste.bat

--- source/teste.bat
+++ source/teste.bat
@@ -1,10 +1,14 @@
 REM Busca compilador
-cd source
-call "c:\program files (x86)\embarcadero\studio\22.0\bin\rsvars.bat"
+call "C:\Program Files (x86)\Embarcadero\Studio\18.0\bin\rsvars.bat"
 REM Busca arquivo do projeto
 FOR /F "delims=" %%i IN ('dir /b "*.dproj"') DO set projeto=%%i
-	@@ -13,4 +14,4 @@ REM executa
+REM Compila projeto
+msbuild %projeto% /p:Config=Debug /t:Build
+@echo off
+REM Extrai nome do projeto
+set nome_projeto=%projeto:.dproj=%
+REM Busca executável
+FOR /F "delims=" %%i IN ('dir /s /b "%nome_projeto%.exe"') DO set executavel=%%i
+REM executa
 %executavel%
 @echo on
-
-pause
