caec43710e8e397d8e780df4d97c28269983b5e9
avelsys <avelinoalessandro@gmail.com>
2023-02-25 02:28:58 -0300

    Update teste.bat

--- source/teste.bat
+++ source/teste.bat
@@ -1,4 +1,10 @@
+REM Busca compilador
 cd source
 call "c:\program files (x86)\embarcadero\studio\22.0\bin\rsvars.bat"
-msbuild sga.dproj
+REM Busca arquivo do projeto
+FOR /F "delims=" %%i IN ('dir /b "*.dproj"') DO set projeto=%%i
+	@@ -13,4 +14,4 @@ REM executa
+%executavel%
+@echo on
+
 pause
